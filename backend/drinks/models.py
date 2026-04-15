9a1388df1017c7eece890fe34968182f11e2c07f
Thomas <t.berghuis@st.hanze.nl>
2026-04-16 00:33:17 +0200

    feat: add Drink model and initial migration

--- backend/drinks/models.py
+++ backend/drinks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Drink(models.Model):
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    barcode = models.CharField(max_length=50, null=True, blank=True, unique=True)
+    rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+    tried = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.brand} - {self.name}"
